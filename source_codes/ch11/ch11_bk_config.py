7551c1a5436da475f11abadbcfcab7f067c3d5aa
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2023-10-26 01:45:46 +1100

    Add files via upload

---
+++ source_codes/ch11/ch11_bk_config.py
@@ -0,0 +1,24 @@
+# ch11_bk_config.py
+import getpass
+import telnetlib
+
+HOST = "192.168.127.134"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"copy running-config tftp://192.168.127.10/running-config\n")
+tn.write(b"\n")
+tn.write(b"\n")
+
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
