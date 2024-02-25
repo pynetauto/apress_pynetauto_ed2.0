e78c3c77d23402361f04553b187357b2fc960e92
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2024-02-25 20:54:29 +1100

    Create sh_vlan1.py

---
+++ source_codes/Part2/ch2_telnet/sh_vlan1.py
@@ -0,0 +1,19 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.127.101"
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"terminal length 0\n")
+tn.write(b"show vlan\n")
+tn.write(b"exit\n")
+print(tn.read_all().decode('ascii'))
