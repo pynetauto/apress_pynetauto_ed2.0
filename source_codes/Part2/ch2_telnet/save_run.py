a36e75546236f497ac14651c9f3d5cd00dd04a5a
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2024-02-25 22:25:05 +1100

    Create save_run.py

---
+++ source_codes/Part2/ch2_telnet/save_run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.10
+
+import getpass
+import telnetlib
+import time
+
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+with open("ip_addresses.txt") as file:
+    for ip in file:
+        print("-"*79) # seperates output and helps readability
+        print(f"Saving config on : {ip}")
+        HOST = ip.strip()
+
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+
+        if password:
+            tn.read_until(b"Password: ")
+            tn.write(password.encode('ascii') + b"\n")
+
+        tn.write(b"wrie memory\n") # save config
+        tn.write(b"exit\n")
+
+        print(tn.read_all().decode('ascii'))
