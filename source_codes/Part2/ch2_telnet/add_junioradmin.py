7945b18fd13953306528e39daebe3277da32faeb
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2024-02-25 21:51:09 +1100

    Create add_junioradmin.py

---
+++ source_codes/Part2/ch2_telnet/add_junioradmin.py
@@ -0,0 +1,30 @@
+import getpass
+import telnetlib
+import time
+
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+with open("ip_addresses.txt") as file:
+    for ip in file:
+        print("Now configuring : " + ip)
+        HOST = ip.strip()
+        
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+        
+        if password:
+            tn.read_until(b"Password: ")
+            tn.write(password.encode('ascii') + b"\n")
+        
+        time.sleep(1)
+        
+        tn.write(b"conf t\n")
+        tn.write(b"username junioradmin privilege 3 password cisco321\n")
+        tn.write(b"privilege exec all level 3 show running-config\n")
+        print("Added a new privilege 3 user")
+        
+        tn.write(b"end\n")
+        tn.write(b"exit\n")
+        print(tn.read_all().decode('ascii'))
