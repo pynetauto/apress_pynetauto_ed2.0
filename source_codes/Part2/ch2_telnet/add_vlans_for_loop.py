64022452de26dffae533f09701b6ae8b8ac1db06
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2024-02-25 20:50:50 +1100

    Create add_vlans_for_loop.py

---
+++ source_codes/Part2/ch2_telnet/add_vlans_for_loop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import getpass
+import telnetlib
+HOST = "192.168.127.101"
+user = input("Enter your username: ")
+password = getpass.getpass()
+tn = telnetlib.Telnet(HOST)
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+       tn.read_until(b"Password: ")
+       tn.write(password.encode('ascii') + b"\n")
+# Get into config mode
+tn.write(b"conf t\n")
+# Adds 5 vlans to the list with for loop
+vlans = [101, 202, 303, 404, 505] # vlans to add in  a list
+for i in vlans: 
+          command_1 = "vlan " + str(i) + "\n" 
+          tn.write(command_1.encode('ascii')) 
+          command_2 = "name PYTHON_VLAN_" + str(i) + "\n" 
+          tn.write(command_2.encode('ascii')) 
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+print(tn.read_all().decode('ascii'))
