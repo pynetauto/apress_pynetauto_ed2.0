4dc25d65e22ada87f69c0ba54d9d560b13c0ec76
Brendan Choi <52733334+pynetauto@users.noreply.github.com>
2024-02-25 21:02:42 +1100

    Update add_vlans_while_loop.py

--- source_codes/Part2/ch2_telnet/add_vlans_while_loop.py
+++ source_codes/Part2/ch2_telnet/add_vlans_while_loop.py
@@ -16,15 +16,15 @@ if password:
 
 tn.write(b"conf t\n")
 
-vlans = [101, 202, 303, 404, 505]  # VLANs to add to the list
+vlans = [101, 202, 303, 404, 505]
 i = 0  # Initialize the index value
 
-while i < len(vlans):  # While 'i' is less than the length of 'vlans'
+while i < len(vlans):
     command_1 = "vlan " + str(vlans[i]) + "\n"
-    tn.write(command_1.encode('ascii'))  # Send 'command_1' with ASCII encoding
+    tn.write(command_1.encode('ascii'))
     command_2 = "name PYTHON_VLAN_" + str(vlans[i]) + "\n"
-    tn.write(command_2.encode('ascii'))  # Send 'command_2' with ASCII encoding
-    i += 1  # Increment 'i' to move to the next VLAN in the list
+    tn.write(command_2.encode('ascii'))
+    i += 1
 
 tn.write(b"end\n")
 tn.write(b"exit\n")
